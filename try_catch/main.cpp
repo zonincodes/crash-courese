d6eab95da5c859597f733e5f3ad051837f5300ef
zonin <business.kariukimartin@gmail.com>
2023-04-03 17:28:31 +0300

    open and write

--- try_catch/main.cpp
+++ try_catch/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #include <system_error>
 #include <cstdio>
+#include <cstring>
 
 struct File {
     File(const char* path, bool write)
@@ -20,3 +21,21 @@ struct File {
 
     FILE *file_pointer;
 };
+
+
+int main()
+{
+    {
+        File file("last_message.txt", true);
+        const auto message = "We apologize for the inconvenience.";
+
+        fwrite(message, strlen(message), 1, file.file_pointer);
+    }
+
+    {
+        File file("last_message.txt", false);
+        char read_message[37]{};
+        fread(read_message, sizeof(read_message), 1, file.file_pointer);
+        printf("Read last message: %s\n", read_message);
+    }
+}
