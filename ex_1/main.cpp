42d3fafa0fcb3b5ed1fc7707579ee4fd9eea4365
zonin <business.kariukimartin@gmail.com>
2023-04-04 09:50:20 +0300

    clockof the longnow type defination

--- ex_1/main.cpp
+++ ex_1/main.cpp
@@ -1,6 +1,40 @@
 #include <cstdlib>
 #include <cstdio>
 
+/*
+First initialize gettysburg to zero
+Initialize the pointer gettyburg_address to the address of gettysburg
+Print the int pointed to vy gettysburg_address
+and the value of gettysburg_address itseld
+Write the value 124 into the memory pointed to by gettysburg_adress and 
+then print the pointed-to value
+and adress
+*/
+struct ClockOfTheLongNow{
+    ClockOfTheLongNow(){
+        set_year(2019);
+    }
+
+    void add_year(){
+        year++;
+    }
+
+    bool set_year(int new_year){
+        if(new_year < 2019) {
+            year = 2019;
+            return false;
+        };
+        year = new_year;
+        return true;
+    }
+
+    int get_year(){
+        return year;
+    }
+
+    private:
+        int year;
+};
 
 int main() {
     int gettyburg{};
