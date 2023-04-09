c9603f0d46d7d6820e9418b5a35ead5cfcf00e5a
zonin <business.kariukimartin@gmail.com>
2023-04-09 09:52:02 +0300

    function transform lambda examples

--- lamda_exp/lamda_exp.cpp
+++ lamda_exp/lamda_exp.cpp
@@ -17,6 +17,26 @@ template <typename Fn>
 void transform(Fn fn, const int* in, int* out, size_t length)
 {
     for(size_t i{}; i < length; i++){
-        out[i] == fn(in[i]);
+        out[i] = fn(in[i]);
+    }
+}
+
+int main()
+{
+    const size_t len{3};
+    int base[]{1, 2, 3}, a[len], b[len], c[len];
+    transform([](int x)
+              { return 1; },
+              base, a, len);
+    transform([](int x)
+              { return x; },
+              base, b, len);
+    transform([](int x)
+              { return 10*x+5; },
+              base, c, len);
+    
+    for ( size_t i{}; i < len; i++)
+    {
+        printf("Element %zd: %d %d %d\n", i, a[i], b[i], c[i]);
     }
 }
