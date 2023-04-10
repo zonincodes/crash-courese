7984c94914da292388621309bd66ed3a054d5295
zonin <business.kariukimartin@gmail.com>
2023-04-10 06:03:45 +0300

    test harness

--- unit_testing/unit_testing.cpp
+++ unit_testing/unit_testing.cpp
@@ -48,6 +48,21 @@ const void assert_that(bool statement, const char* message)
     if(!statement) throw std::runtime_error{message};
 }
 
+void initial_speed_is_xero(){
+    AutoBrake auto_brake{[](const BrakeCommand&){}};
+    assert_that(auto_brake.get_speed_mps() == 0L, "speed not equal 0");
+}
+
+// test harness
+void run_test(void(*unit_test)(), const char* name){
+    try{
+        unit_test();
+        printf("[+] Test %s successful.\n", name);
+    } catch (const std::exception &e){
+        printf("[-] Test failure in %s. %s.\n", name, e.what());
+    }
+}
+
 int main() {
     ServiceBus bus;
 
