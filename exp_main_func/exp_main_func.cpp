2085c11c69d904acaec369aa33d9cfca60acec7c
zonin <business.kariukimartin@gmail.com>
2023-04-10 09:21:05 +0300

    autobrake initial test

--- exp_main_func/exp_main_func.cpp
+++ exp_main_func/exp_main_func.cpp
@@ -1,10 +1,19 @@
 #include <cstdio>
 #include <cstdint>
 
+struct HelloWorld
+{
+    int one; 
+    int two;
+};
 int main(int argc, char** argv){
     printf("Arguments: %d\n", argc);
     for(size_t i{1}; i < argc; i++)
     {
         printf("%zd: %s\n", i, argv[i]);
     }
+
+    // 
+    HelloWorld greets{1, 3};
+    printf("one: %d | two: %d\n", greets.one, greets.two);
 }
--- unit_testing/unit_testing.cpp
+++ unit_testing/unit_testing.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <stdexcept>
+#include <cstdint>
 struct SpeedUpdate
 {
     double velocity_mps;
@@ -25,11 +26,23 @@ struct ServiceBus
 template <typename T>
 struct AutoBrake
 {
-    AutoBrake(const T &publish) : collision_threshold_s{5}, speed_mps{},publish { publish} {}
+    AutoBrake(const T &publish) : collision_threshold_s{5}, speed_mps{}, publish{publish} 
+    {
+
+    }
+
     void observe(const SpeedUpdate& cd) {
         speed_mps = cd.velocity_mps;
     }
-    void observe(const CarDetected& cd) {}
+    void observe(const CarDetected& cd) {
+        const auto relative_velocity_mps = speed_mps - cd.velocity_mps;
+        const auto time_to_collison_s = cd.distance_m / relative_velocity_mps;
+
+        if(time_to_collison_s > 0 && 
+            time_to_collison_s <= collision_threshold_s){
+            publish(BrakeCommand{time_to_collison_s});
+        }
+    }
 
     void set_collision_threshold_s(double x){
         if(x < 1) throw std::runtime_error{"Collision less than 1."};
@@ -40,17 +53,17 @@ struct AutoBrake
         return speed_mps;
     }
 
-    double get_collision_threshold_s(){
+    double get_collision_threshold_s() const {
         return collision_threshold_s;
     }
 
     private:
-        const T& publish;
         double collision_threshold_s;
         double speed_mps;
+        const T& publish;
 };
 
-const void assert_that(bool statement, const char* message)
+void assert_that(bool statement, const char* message)
 {
     if(!statement) throw std::runtime_error{message};
 }
@@ -61,7 +74,7 @@ void initial_speed_is_xero(){
 }
 
 // requiremnt: Default Collision Threshold is Five
-void initial_seinstivity_is_five()
+void initial_sensitivity_is_five()
 {
     AutoBrake auto_brake{[](const BrakeCommand&){}};
     assert_that(auto_brake.get_collision_threshold_s() == 5L, "sensitivity is not 5");
@@ -91,6 +104,19 @@ void speed_is_saved() {
     assert_that(0L == auto_brake.get_speed_mps(), "speed not saved to 0");
 }
 
+// Requirement: AutoBrake Publishes a BrakeCommand When Collisison Detected
+
+void alert_when_imminent() {
+    int brake_commands_published{};
+    AutoBrake auto_brake{[&brake_commands_published](const BrakeCommand&){
+            brake_commands_published++;
+        }};
+    auto_brake.set_collision_threshold_s(10L);
+    auto_brake.observe(SpeedUpdate{100L});
+    auto_brake.observe(CarDetected{100L, 0L});
+    assert_that(brake_commands_published == 1, "brake commands published not one");
+}
+
 
 // test harness
 void run_test(void(*unit_test)(), const char* name){
@@ -104,7 +130,8 @@ void run_test(void(*unit_test)(), const char* name){
 
 int main() {
     run_test(initial_speed_is_xero, "initial speed is 0");
-    run_test(initial_seinstivity_is_five, "initial sensitivity is 5");
+    run_test(initial_sensitivity_is_five, "initial sensitivity is 5");
     run_test(sensitivity_greater_than_1, "sensitivity greater than 1");
     run_test(speed_is_saved, "speed is saved");
+    run_test(alert_when_imminent, "alert when imminent");
 }
